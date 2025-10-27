eda70bea41e753c81d96c73a34b176acec8f660b
pujithareddykethu2302 <kethupujithareddy52@gmail.com>
2025-10-27 11:16:32 +0530

    feat: dashboard styling

--- src/components/Fed/Dashboard/Dashboard.tsx
+++ src/components/Fed/Dashboard/Dashboard.tsx
@@ -5,8 +5,8 @@ import { useNotes } from "../../Common/NotesContext";
 import { days, months } from "../../Common/Constants";
 import NotesImg from "../.../../../../assets/Images/Dashboard/Notes-pana.svg";
 import { useNavigate } from "react-router-dom";
-import StaticDatePickerLandscape from "../../Common/Calender";
 import DateCalendarServerRequest from "../../Common/Calender";
+import EastIcon from "@mui/icons-material/East";
 
 const Dashboard = () => {
   const navigate = useNavigate();
@@ -46,58 +46,63 @@ const Dashboard = () => {
 
   console.log("date", TodayDate, month, Week);
   return (
+    <>
     <div className="bg-gray-100 min-h-screen p-10">
-      <p className="text-[25px] font-semibold ">Dashboard</p>
+      <h1 className="text-2xl font-bold text-[#563A9C]">
+        Welcome to <span className="text-[#2b2828]">FED</span>
+      </h1>
+      <p className="text-lg text-[#2b2828] font-medium mt-[1rem]">
+        Turn Every Day into Progress — One Challenge at a Time.
+      </p>
+
       <div className=" justify-between flex flex-col sm:flex-row gap-1 mb-2">
-        <div className="bg-white rounded-xl shadow-md p-6 mb-2 mt-8 flex flex-col sm:flex-row justify-between w-[100%] lg:w-[70%]">
+        <div className="bg-gradient-to-r from-[#3E2C7A] via-[#563A9C] to-[#7A5AE0] rounded-xl shadow-sm mt-8 px-4 py-4 flex flex-col sm:flex-row justify-between w-[100%] lg:w-[70%]">
           <div className="mt-[2rem]">
-            <p className="text-2xl sm:text-4xl font-bold">
-              {GreetingMessages
-                ? `Hello!, ${GreetingMessages.greeting}`
-                : "Hello!"}
+            <p className="text-2xl sm:text-4xl font-bold text-white/90">
+              {GreetingMessages ? ` ${GreetingMessages.greeting},` : "Hello!"}
             </p>
-            <p className="mt-[1rem] text-[20px] font-[500] text-[#563A9C]">
+            <p className="mt-[1rem] text-[20px] font-[500] text-white/90">
               {GreetingMessages
                 ? caption
                 : "Your journey to mastery begins today — 30 days, endless possibilities."}
             </p>
-            <button className="px-4 py-2 bg-[#563A9C] text-white rounded hover:bg-[#472F85] mt-[2rem]">
-              Let’s Go!
+            <button className=" font-[700] px-4 py-2 text-[#563A9C] bg-white rounded hover:bg-[#472F85] mt-[2rem]">
+              Let’s Go! <EastIcon className="ml-[1rem]" />
             </button>
           </div>
-          <div>
-            <img src={StartImage} className="w-70 h-70" />
+          <div className="mt-8">
+            <img src={StartImage} className="w-150 h-70" />
           </div>
         </div>
         <div className="mt-8 rounded-2xl shadow-lg  flex flex-col w-full lg:w-[27%]">
-           <div className="bg-[#563A9C] rounded-lg px-4 py-2 flex justify-center items-center">
-              <p className="text-lg font-semibold text-white">My Check-In Tracker</p>
-           </div>
+          <div className="bg-[#563A9C] rounded-t-lg px-4 py-4 flex justify-center items-center">
+            <p className="text-lg font-semibold text-white">
+              My Check-In Tracker
+            </p>
+          </div>
 
           <DateCalendarServerRequest />
-    
-       
         </div>
       </div>
       <div className=" justify-between flex flex-col sm:flex-row gap-1 mb-2 w-[100%]">
-        <div className=" flex w-[100%] lg:w-[70%]">
-          <div className="bg-white rounded-xl shadow-md p-6 my-8  w-[100%] lg:w-[50%] mr-[2rem] transition-transform hover:scale-[1.01]">
+        <div className=" flex w-[100%] lg:w-[70%]  flex-col sm:flex-row">
+          <div className="bg-white rounded-xl shadow-md p-6 my-4  w-[100%] lg:w-[50%] mr-[2rem] transition-transform hover:scale-[1.01]">
             <p>status graph </p>
             <button className="px-4 py-2 bg-[#563A9C] text-white rounded hover:bg-[#472F85] mt-[2rem]">
-              Let’s Go!
+              Let’s Go! <EastIcon className="ml-[1rem]" />
             </button>
           </div>
-          <div className="bg-white rounded-2xl shadow-md p-6 my-8 w-full lg:w-1/2 transition-transform hover:scale-[1.01]">
-            <div className="bg-[#563A9C] rounded-lg px-4 py-2 flex justify-between items-center">
+          <div className="bg-white rounded-2xl shadow-md  my-4 w-full lg:w-1/2 transition-transform hover:scale-[1.01]">
+            <div className="bg-[#563A9C] rounded-t-lg px-4 py-4 flex justify-between items-center">
               <p className="text-lg font-semibold text-white">Notes Hub</p>
               <button
                 onClick={() => navigate("/CodePad-page")}
                 className="text-sm text-white/80 hover:text-white underline"
               >
-                View All
+                View All <EastIcon className="ml-[1rem]" />
               </button>
             </div>
-            <div className="mt-4">
+            <div className="mt-2 p-4">
               {randomItems.map((note) => (
                 <button
                   key={note.title}
@@ -112,26 +117,38 @@ const Dashboard = () => {
               ))}
             </div>
             <div className="flex justify-center items-center mt-6">
-              <img src={NotesImg} alt="Notes" className="w-40 h-40 " />
+              <img src={NotesImg} alt="Notes" className="w-50 h-50 " />
             </div>
           </div>
         </div>
-        <div className="flex flex-col justify-between w-[27%]">
-          <div className="bg-white rounded-xl shadow-md p-6 mb-8 mt-8  w-[100%] transition-transform hover:scale-[1.01]">
-            <p>plannere</p>
-            <button className="px-4 py-2 bg-[#563A9C] text-white rounded hover:bg-[#472F85] mt-[2rem]">
-              Let’s Go!
+        <div className="flex flex-row justify-between lg:w-[27%] w-full lg:flex-col ">
+          <div className="bg-[#563A9C] rounded-xl shadow-md p-6 mb-4 mt-4 w-[100%] transition-transform hover:scale-[1.01]">
+            <p className="text-white text-[16px] font-[700]">
+              Plan your day, track your progress, and schedule your 30-day
+              challenge tasks.
+            </p>
+            <button className="px-4 py-2 text-[#563A9C] bg-white rounded hover:bg-[#d9d5e4] mt-[2rem] font-[700]">
+              Open Planner <EastIcon className="ml-[1rem]" />
             </button>
           </div>
-          <div className="bg-white rounded-xl shadow-md p-6 mb-8 mt-8  w-[100%] transition-transform hover:scale-[1.01]">
-            <p>Other challenge </p>
-            <button className="px-4 py-2 bg-[#563A9C] text-white rounded hover:bg-[#472F85] mt-[2rem]">
-              Let’s Go!
+          <div className="bg-[#563A9C] rounded-xl shadow-md p-6 mb-4 mt-4  w-[100%] transition-transform hover:scale-[1.01]">
+            <p className="text-white text-[16px] font-[700]">
+              Jump into your coding challenges, track progress, and level up
+              your skills.
+            </p>
+            <button className="px-4 py-2 text-[#563A9C] bg-white rounded hover:bg-[#d9d5e4] mt-[2rem] font-[700]">
+              Let’s Go! <EastIcon className="ml-[1rem]" />
             </button>
           </div>
         </div>
       </div>
     </div>
+     <div className="text-center py-16 bg-gradient-to-r from-[#563A9C] to-[#7E5CCB] text-white">
+        <p className="text-2xl sm:text-3xl font-semibold italic tracking-wide">
+          “Small Steps. Big Progress.”
+        </p>
+      </div>
+    </>
   );
 };
 
---
+++ src/components/Fed/Dashboard/StatusGraph.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StatusGraph = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default StatusGraph
