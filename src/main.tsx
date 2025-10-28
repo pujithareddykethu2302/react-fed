5d16067dc8c5567f79732bf0bc48d00cdf0abd54
pujithareddykethu2302 <kethupujithareddy52@gmail.com>
2025-10-28 10:47:30 +0530

    feat: base route

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import ReactDays from "./components/Fed/React Challeneg/ReactDays.tsx";
 import Layout from "./components/Layout.tsx";
 import Dashboard from "./components/Fed/Dashboard/Dashboard.tsx";
@@ -15,55 +16,32 @@ import { NotesProvider } from "./components/Common/NotesContext.tsx";
 import MoreChallenges from "./components/Fed/MoreChallenges/MoreChallenges.tsx";
 import CategoryChallenges from "./components/Fed/MoreChallenges/CategoryChallenges.tsx";
 
-
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { index: true, element: <Dashboard /> },
+        { path: "/30-days-challenge", element: <ReactDays /> },
+        { path: "/30-days-challenge/challenge-details/:id", element: <ChallengeDetails /> },
+        { path: "/about-us", element: <AboutUs /> },
+        { path: "/contact-us", element: <ContactUS /> },
+        { path: "/my-planner", element: <Planner /> },
+        { path: "/CodePad-page", element: <Resources /> },
+        { path: "/More-hands-on-Challenges", element: <MoreChallenges /> },
+        { path: "/More-hands-on-Challenges/:categoryName", element: <CategoryChallenges /> },
+        {
+          path: "/More-hands-on-Challenges/:categoryName/challenge-details/:id",
+          element: <ChallengeDetails />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <Dashboard />,
-      },
-      {
-        path: "/30-days-challenge",
-        element: <ReactDays />,
-      },
-      {
-        element: <ChallengeDetails />,
-        path: "/30-days-challenge/challenge-details/:id",
-      },
-      {
-        element: <AboutUs />,
-        path: "/about-us",
-      },
-      {
-        element: <ContactUS />,
-        path: "contact-us",
-      },
-      {
-        element: <Planner />,
-        path: "/my-planner",
-      },
-      {
-        element: <Resources />,
-        path: "/CodePad-page",
-      },
-      {
-        element: <MoreChallenges />,
-        path: "/More-hands-on-Challenges",
-      },
-      {
-        element: <CategoryChallenges />,
-        path: "/More-hands-on-Challenges/:categoryName",
-      },
-      {
-        element: <ChallengeDetails />,
-        path: "/More-hands-on-Challenges/:categoryName/challenge-details/:id",
-      },
-    ],
-  },
-]);
+    basename: "/react-fed", 
+  }
+);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,5 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     react()],
-    base:"/FED-challenges"
+    base:"/react-fed"
 })
